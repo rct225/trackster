4c1931022e1556d1c0e839ccc19e07f1acc2e9cc
Robert Tuck <robtuck@gmail.com>
2017-10-12 10:31:08 -0400

    initial commit

---
+++ resources/js/trackster.js
@@ -0,0 +1,30 @@
+var Trackster = {};
+
+const API_KEY = config.API_KEY;
+
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#search-input').val());
+  });
+
+})
+/*
+  Given an array of track data, create the HTML for a Bootstrap row for each.
+  Append each "row" to the container in the body to display all tracks.
+*/
+Trackster.renderTracks = function(tracks) {
+
+};
+
+/*
+  Given a search term as a string, query the LastFM API.
+  Render the tracks given in the API query response.
+*/
+Trackster.searchTracksByTitle = function(title) {
+  $.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
+    success: function(response) {
+      console.log(response);
+    }
+  })
+};
