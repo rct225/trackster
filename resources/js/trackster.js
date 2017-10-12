2c25b5f7c3268e504b556d14f99a72fcc4a24200
Robert Tuck <robtuck@gmail.com>
2017-10-12 15:13:16 -0400

    looked up album name.  asynchronous mess :)

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -13,7 +13,44 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  $('#track-list').empty("");
+  for (let i = 0; i < tracks.length; i++) {
+    function callback(data){
+      var albumArt = tracks[i].image[1]["#text"];
+      var albumMbid = tracks[i].mbid;
+      var albumName;
+      if (data.error) {
+        albumName = "";
+      } else {
+        albumName = data.track.album.title;
+      }
+      //albumName = data;
+      var trackRowHtml = `<div class="row track">
+        <div class="col-xs-offset-1 col-xs-1">
+          <a href="${tracks[i].url}" target="_blank">
+            <i class="fa fa-play-circle-o fa-2x play-button" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div class="col-xs-2">${tracks[i].name}</div>
+        <div class="col-xs-2">${tracks[i].artist}</div>
+        <div class="col-xs-2">${albumName}</div>
+        <div class="col-xs-2">
+          <img src="${albumArt}">
+        </div>
+        <div class="col-xs-2">${tracks[i].listeners}</div>
+      </div>`;
+      $('#track-list').append(trackRowHtml);
+    };
 
+    $.ajax ({
+        //async: false,
+        // url: "http://ws.audioscrobbler.com/2.0/?method=track.getinfo&mbid=" + albumMbid + "&api_key=" + API_KEY + "&format=json",
+        url: "http://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist=" + tracks[i].artist + "&track=" + tracks[i].name + "&api_key=" + API_KEY + "&format=json",
+    }).done( function(data) {
+      callback(data);
+    })
+    //console.log(albumName);
+  }
 };
 
 /*
@@ -25,6 +62,7 @@ Trackster.searchTracksByTitle = function(title) {
     url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key=" + API_KEY + "&format=json",
     success: function(response) {
       console.log(response);
+      Trackster.renderTracks(response.results.trackmatches.track);
     }
   })
 };
